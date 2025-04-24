e64de243ae138ba658cac8f2a1422dd02caddc78
CJ <cjwarhyl@gmail.com>
2025-04-24 10:30:33 +0800

    Fix BLACKPEARL logo by using inline SVG instead of Image component

--- src/app/components/BlackPearlLogo.tsx
+++ src/app/components/BlackPearlLogo.tsx
@@ -1,15 +1,11 @@
-import Image from 'next/image';
-
 export default function BlackPearlLogo() {
   return (
     <div className="fixed bottom-4 right-4 z-10">
-      <Image
-        src="/blackpearl-logo.svg"
-        alt="BLACKPEARL Logo"
-        width={120}
-        height={30}
-        priority
-      />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 40" width="120" height="30">
+        <text x="0" y="30" fontFamily="Arial, sans-serif" fontWeight="bold" fontSize="24">
+          <tspan fill="#ff0000">B</tspan><tspan fill="#000000">LACKPEARL</tspan>
+        </text>
+      </svg>
     </div>
   );
 } 
